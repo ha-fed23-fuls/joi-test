1b927afdf0a21384a22c00b8206abae9df8d332b
David Andersson <lejonmanen@gmail.com>
2024-10-02 12:02:29 +0200

    Testfall för strict number

---
+++ script.ts
@@ -0,0 +1,29 @@
+import Joi from 'joi'
+
+interface X {
+	a: number;
+}
+
+let x1 = { a: 1 }, x2 = { a: '2' }, x3 = { a: 'fail' }
+let cases = [x1, x2, x3]
+
+let schema1 = Joi.object({
+	a: Joi.number()
+})
+let schema2 = Joi.object({
+	a: Joi.number().strict()
+})
+function f(x:any) {
+	return x?.error ? 'FAIL' : 'ok'
+}
+
+console.log('Validerar testfall utan strict:')
+cases.forEach(c => {
+	console.log('-> ', f(schema1.validate(c)))
+})
+console.log('')
+console.log('-------------------------------')
+console.log('Validerar testfall med strict:')
+cases.forEach(c => {
+	console.log('-> ', f(schema2.validate(c)))
+})
